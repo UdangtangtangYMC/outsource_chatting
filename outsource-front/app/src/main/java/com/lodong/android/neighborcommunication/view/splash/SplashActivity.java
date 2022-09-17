2b39e3fec2e468ba9da04554c533118833cbc672
wsm9175 <76678911+wsm9175@users.noreply.github.com>
2022-09-17 14:19:27 +0900

    add activity - SplashActivity

---
+++ outsource-front/app/src/main/java/com/lodong/android/neighborcommunication/view/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.lodong.android.neighborcommunication.view.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lodong.android.neighborcommunication.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
