95cf7afc1d31433d5223a237c87e2d16d34fee7e
wsm9175 <wsm9175@gmail.com>
2022-09-25 02:45:35 +0900

    add dialog - sign up complete

--- outsource-front/app/src/main/java/com/lodong/android/neighborcommunication/view/signup/SignUpViewModel.java
+++ outsource-front/app/src/main/java/com/lodong/android/neighborcommunication/view/signup/SignUpViewModel.java
@@ -1,16 +1,24 @@
 package com.lodong.android.neighborcommunication.view.signup;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.lifecycle.ViewModel;
 
 import com.lodong.android.neighborcommunication.R;
+import com.lodong.android.neighborcommunication.data.UserInfo;
 import com.lodong.android.neighborcommunication.repository.Repository;
 import com.lodong.android.neighborcommunication.repository.RepositoryImpl;
 import com.lodong.android.neighborcommunication.repository.model.MemberDTO;
+import com.lodong.android.neighborcommunication.utils.preferences.PreferenceManager;
 import com.lodong.android.neighborcommunication.view.callback.SignUpCallBack;
+import com.lodong.android.neighborcommunication.view.login.LoginActivity;
 
 import java.lang.ref.WeakReference;
 
@@ -37,9 +45,21 @@ public class SignUpViewModel extends ViewModel {
         return new SignUpCallBack() {
             @Override
             public void onSuccess() {
-                Dialog dialog = new Dialog(mRef.get());
-                dialog.setContentView(R.layout.dialog_sign_up_complete);
-                dialog.show();
+                View dialogView = mRef.get().getLayoutInflater().inflate(R.layout.dialog_sign_up_complete, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(mRef.get());
+                builder.setView(dialogView);
+
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+                Button okButton = dialogView.findViewById(R.id.btn_ok);
+
+                okButton.setOnClickListener(view -> {
+                    mRef.get().startActivity(new Intent(mRef.get(), LoginActivity.class));
+                    alertDialog.dismiss();
+                });
+
             }
 
             @Override
