e421a40d5189bc2157c496cfbe0de0f616817651
hyeonho <6190606@naver.com>
2022-10-02 21:40:47 +0900

    rollback-to-no-security

--- outsource-backend/src/main/java/com/hyunho9877/outsource/filter/FilterChainValidator.java
+++
@@ -1,5 +0,0 @@
-package com.hyunho9877.outsource.filter;
-
-public interface FilterChainValidator {
-    boolean validate(String url);
-}
--- outsource-backend/src/main/java/com/hyunho9877/outsource/utils/jwt/ApplicationJwtParser.java
+++
@@ -1,5 +0,0 @@
-package com.hyunho9877.outsource.utils.jwt;
-
-public interface ApplicationJwtParser {
-    String getSubject(String token);
-}
